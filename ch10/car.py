e342acb7bc6d29ea3a5336163018755b2ee184d3
amon-ucsm <marjackmm@gmail.com>
2025-07-31 23:44:21 +0630

    Update car.py

--- ch10/car.py
+++ ch10/car.py
@@ -1,9 +1,10 @@
-class Car():
+class Car(): # Class definition for a car
+    """A simple attempt to model a car."""
     def __init__ (self, make, model, year):
         self.make = make
         self.model = model
         self.year = year
-        self.odometer_reading = 0
+        self.odometer_reading = 0 # Sets default calue for an attrubute
         
     def get_descriptive_name(self):
         long_name = str(self.year) + ' ' + self.make + ' ' + self.model
@@ -15,3 +16,5 @@ class Car():
 my_new_car = Car('bmw', 'm5', 2020)
 print(my_new_car.get_descriptive_name())
 my_new_car.read_odometer()
+my_new_car.odometer_reading = 23 # Directly modifying the attribute
+my_new_car.read_odometer() # Reading the updated odometer value
