e74466e0467d8d2fca276c13cb5e16340ea49233
amon-ucsm <marjackmm@gmail.com>
2025-08-02 21:54:20 +0630

    Adding ex9.2 from chapter9

---
+++ ch9/ex9_2.py
@@ -0,0 +1,9 @@
+from ex9_1 import Restaurant
+
+restaurant1 = Restaurant('Kyaw Kyaw', 'BBQ')
+restaurant2 = Restaurant('Lucky', 'liquier')
+restaurant3 = Restaurant('seven eleven', 'store')
+
+restaurant1.describe_restaurant()
+restaurant2.describe_restaurant()
+restaurant3.describe_restaurant()
