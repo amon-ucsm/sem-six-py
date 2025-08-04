b8098bb6af2c63f6424f5f7a281f1060b5ef9a77
amon-ucsm <marjackmm@gmail.com>
2025-08-04 15:09:51 +0630

    Update remember_me.py

--- ch10/remember_me.py
+++ ch10/remember_me.py
@@ -1,8 +1,24 @@
-import json
+"""import json
 
 username = input("What is your name: ")
 filename = 'ch10/username.json'
 
 with open(filename, 'w') as f_obj:
     json.dump(username, f_obj)
-    print("We'll remember you when you come back, " + username + "!")
+    print("We'll remember you when you come back, " + username + "!")"""
+    
+# Update status
+import json
+
+filename = 'ch10/username.json'
+
+try:
+    with open(filename) as f_obj:
+        username = json.load(f_obj)
+except FileNotFoundError:
+    username = input("What is yout name: ")
+    with open(filename, 'w') as f_obj:
+        json.dump(username, f_obj)
+        print("We'll remember you when you come back, " + username + "!")
+else:
+    print("Welcome back " + username + "!")
