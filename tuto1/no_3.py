61d9124d9e3116dce63124cb25716300bc3ffc00
amon-ucsm <143180725+amon-ucsm@users.noreply.github.com>
2025-08-05 22:46:20 +0630

    Delete tuto1 directory

--- tuto1/no_3.py
+++
@@ -1,8 +0,0 @@
-fruits = ['Apple', 'Avocado', 'Banana', 'Watermelon', 'Grape', 'Pineapple', 'Strawberry', 'Blueberry', 'Raspberry', 'Orange', 'Mango',
-          'Cherry']
-print(fruits)
-
-favorite_fruits = ['Avocado', 'Watermelon', 'Pear', 'Jackfruit', 'Guava']
-for fruit in favorite_fruits:
-    if fruit in fruits:
-        print(f"I really like {fruit}")
