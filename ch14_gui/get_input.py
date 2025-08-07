119f02678b14828f5453289c20a08cf82e011e00
amon-ucsm <marjackmm@gmail.com>
2025-08-07 09:44:41 +0630

    Update get_input.py and add pack.py

--- ch14_gui/get_input.py
+++ ch14_gui/get_input.py
@@ -6,9 +6,20 @@ def quit(top):
 
 def display():
     name = var.get()
+    label1.configure(text="Hello " + name)
 
 
 top = tkinter.Tk()
 var = tkinter.StringVar()
 entry1 = tkinter.Entry(top, textvariable= var, width= 20)
 entry1.grid(row=0, column=0)
+
+label1 = tkinter.Label(top, text="", width= 20)
+label1.grid(row=0 ,column= 1)
+
+button1 = tkinter.Button(top, text="Display", command=display)
+button1.grid(row=1, column= 0)
+
+button2 = tkinter.Button(top, text="Quit", command=lambda top=top:quit(top), bg="red", fg="white")
+button2.grid(row=1, column=1)
+top.mainloop()
