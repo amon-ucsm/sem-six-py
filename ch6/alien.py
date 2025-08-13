d8ce77fcdceabe9d984c6215d1ece1c7a5e5cdf2
amon-ucsm <marjackmm@gmail.com>
2025-08-13 20:50:55 +0630

    Update ch6

--- ch6/alien.py
+++ ch6/alien.py
@@ -3,13 +3,21 @@ alien_0 = {
     'point': 5,
 }
 
+alien_1 = {'color': 'yellow', 'point': 10,}
+alien_2 = {'color': 'red', 'point': 15,}
+
+aliens = [alien_0, alien_1, alien_2]
+for alien in aliens:
+    print(alien)
+
 """print(alien_0['color'])
 #print(alien_0['point'])
 new_points = alien_0['point']
 print("You just earned " + str(new_points) + "points!")"""
 
+"""
 # Addding key-value pairs
 print(alien_0)
 alien_0['x_position'] = 0
 alien_0['y_position'] = 25
-print(alien_0)
+print(alien_0) """
